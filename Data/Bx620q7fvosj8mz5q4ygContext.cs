23cc8ab769ad9fe618ce7de41676eda321b9c631
medi77na <medinastevenurregodavid@gmail.com>
2024-08-27 21:52:11 -0500

    feat: se carga el scafold desde la BD.

---
+++ Data/Bx620q7fvosj8mz5q4ygContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestionUsuarios.Data;
+
+public partial class Bx620q7fvosj8mz5q4ygContext : DbContext
+{
+    public Bx620q7fvosj8mz5q4ygContext()
+    {
+    }
+
+    public Bx620q7fvosj8mz5q4ygContext(DbContextOptions<Bx620q7fvosj8mz5q4ygContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=bx620q7fvosj8mz5q4yg-mysql.services.clever-cloud.com;port=3306;database=bx620q7fvosj8mz5q4yg;user=uygmpftpmrbimy9j;password=BBkLZkzXjjDyTs5zvoy3", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.15-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(45)
+                .HasColumnName("apellido");
+            entity.Property(e => e.FechaNacimiento).HasColumnName("fecha_nacimiento");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(45)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionUsuarios.Data;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public DateOnly FechaNacimiento { get; set; }
+}
